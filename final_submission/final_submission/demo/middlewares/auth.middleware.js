45076304ccab56e1352996f80663198f399e63e4
Akshara266 <155669836+Akshara266@users.noreply.github.com>
2026-01-15 21:39:31 -0600

    Implementation of a custom Authentication Middleware

--- final_submission/final_submission/demo/middlewares/auth.middleware.js
+++ final_submission/final_submission/demo/middlewares/auth.middleware.js
@@ -1,1 +1,21 @@
+const ApiError = require("../utils/apiError");
+const { verifyToken } = require("../utils/jwt");
 
+module.exports = (req, res, next) => {
+  try {
+    const header = req.headers.authorization || "";
+    const [type, token] = header.split(" ");
+
+    if (type !== "Bearer" || !token) {
+      throw new ApiError(401, "Missing or invalid Authorization header");
+    }
+
+    const decoded = verifyToken(token);
+    // decoded should contain { user_id, role, iat, exp }
+    req.user = { user_id: decoded.user_id, role: decoded.role };
+
+    return next();
+  } catch (err) {
+    return next(new ApiError(401, "Invalid or expired token"));
+  }
+};
