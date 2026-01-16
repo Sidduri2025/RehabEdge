c552fcaad8564af40e816230b7c94e330f7f4c15
Sidduri2025 <vaishusidduri@gmail.com>
2026-01-15 21:06:43 -0600

    Implementation of User Registration and Login Authentication.

--- final_submission/final_submission/demo/controllers/auth.controllers.js
+++ final_submission/final_submission/demo/controllers/auth.controllers.js
@@ -1,1 +1,48 @@
+const authService = require("../services/auth.service");
+const passwordResetService = require("../services/passwordReset.service");
+
+exports.register = async (req, res, next) => {
+  try {
+    const result = await authService.register(req.body);
+    res.status(201).json({ success: true, data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const result = await authService.login(req.body);
+    res.status(200).json({ success: true, data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    const result = await passwordResetService.requestPasswordReset(req.body);
+    res.status(200).json({ success: true, message: result.message });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.verifyOtp = async (req, res, next) => {
+  try {
+    const result = await passwordResetService.verifyOtp(req.body);
+    res.status(200).json({ success: true, message: result.message });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.resetPassword = async (req, res, next) => {
+  try {
+    const result = await passwordResetService.resetPassword(req.body);
+    res.status(200).json({ success: true, message: result.message });
+  } catch (err) {
+    next(err);
+  }
+};
 
